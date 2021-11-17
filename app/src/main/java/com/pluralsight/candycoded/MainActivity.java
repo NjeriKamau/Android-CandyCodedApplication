2a58f9d684a0703f323ef75971154095317bf676
NjeriKamau <mukurutracey@gmail.com>
2021-11-17 12:36:47 +0300

    Task was to: Start the Store Information Activity. This was done using implicit and explicit intents.

--- app/src/main/java/com/pluralsight/candycoded/MainActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/MainActivity.java
@@ -1,5 +1,8 @@
 package com.pluralsight.candycoded;
 
+import static android.content.ClipData.newIntent;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.ContentValues;
 import android.content.Intent;
@@ -9,6 +12,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -78,6 +82,13 @@ public class MainActivity extends AppCompatActivity {
     MenuInflater inflater = getMenuInflater();
     inflater.inflate(R.menu.main, menu);
     return true;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+    Intent infoIntent= new Intent(this, InfoActivity.class);
+    startActivity(infoIntent);
+    return super.onOptionsItemSelected(item);
   }
   // ***
   // TODO - Task 1 - Show Store Information Activity
